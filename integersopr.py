64ad4ea2553bcfafb9033e42094b567e7afa9641
gevishahari <42989952+gevishahari@users.noreply.github.com>
2018-09-14 09:20:22 +0530

    Add files via upload

---
+++ integersopr.py
@@ -0,0 +1,11 @@
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+if(x>y):
+	print("x is the largest number")
+if(y>x):
+    print("y is the largest number")
+
+if (x==y):
+   print("x is equal to y")
+
+   print("they are equal")
